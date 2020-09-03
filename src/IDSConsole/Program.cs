132392b2304bab7c547c54a4fed537712c9a9db2
cdueso <cdueso@userzoom.com>
2020-09-03 11:44:38 +0200

    inital setup

---
+++ src/IDSConsole/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IDSConsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+        }
+    }
+}
