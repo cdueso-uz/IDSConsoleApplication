c5a9776392cda225fb5ac32fbb717a76308ab479
cdueso <cdueso@userzoom.com>
2020-09-04 11:30:37 +0200

    logout ok

--- src/IDSConsole/Program.cs
+++ src/IDSConsole/Program.cs
@@ -12,12 +12,12 @@ namespace IDSConsole
 {
     public class Program
     {
-        //private static string hostAddress = "192.168.7.250:5001";
         private static string hostAddress = "https://localhost:5001";
+        //private static string hostAddress = "https://localhost:5001";
 
         public static async Task Main(string[] args)
         {
-            var client = new HttpClient();
+            var clientUICookie = new HttpClient();
 
             var login = new LoginRequest()
             {
@@ -32,18 +32,18 @@ namespace IDSConsole
             try
             {
                 //1 - Send Authenticate call
-                var responseAuthenticate = await client.PostAsync($"{hostAddress}/co/authenticate", request);
+                var responseAuthenticate = await clientUICookie.PostAsync($"{hostAddress}/co/authenticate", request);
                 var cookies = responseAuthenticate.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
 
                 //2 - connect/Authorize
                 var authorizeQueryString = $"{hostAddress}/authorize?client_id=client&scope=openid profile email offline_access&" +
                                            $"response_type=code&redirect_uri=https://www.google.com&state=Tj0xpy1Q" +
                                            $"&connection=UZManagerUsersMigration-dev&realm=UZManagerUsersMigration-dev&login_ticket=R-SvDuFgyPbjKRdBSyGXPN4Nl0m4euZP";
-                var responseAuthorize = await client.GetAsync(authorizeQueryString);
+                var responseAuthorize = await clientUICookie.GetAsync(authorizeQueryString);
                 var codeAuthorize = QueryHelpers.ParseQuery(responseAuthorize.RequestMessage.RequestUri.Query)["code"].First();
 
                 //3 - connect/Token
-                var tokenClient = new HttpClient();
+                var clientBackend = new HttpClient();
 
                 var keyValuePairs = new List<KeyValuePair<string, string>>
                 {
@@ -59,9 +59,14 @@ namespace IDSConsole
                     Content = new FormUrlEncodedContent(keyValuePairs)
                 };
 
-                var responseToken = await tokenClient.SendAsync(tokenRequest);
-
+                var responseToken = await clientBackend.SendAsync(tokenRequest);
                 Console.WriteLine(responseToken.Content.ReadAsStringAsync().Result);
+
+                //4 - Logut
+                var logoutQueryString = $"{hostAddress}/v2/logout?client_id=client&returnTo=https://www.google.com&auth0Client=patata";
+                var responseLogout = await clientUICookie.GetAsync(logoutQueryString);
+                var cookiesLogout = responseAuthenticate.Headers.SingleOrDefault(header => header.Key == "Set-Cookie").Value;
+
             }
             catch (Exception ex)
             {
